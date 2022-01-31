370138198f1b25ca9a2bc611036372dbe625b7a0
ZaidAhmedMohammed <96975671+ZaidAhmedMohammed@users.noreply.github.com>
2022-01-31 20:53:12 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,37 @@
+canvas = document.getElementById("myCanvas")
+color = "red";
+
+ctx= canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 2;
+ctx.arc(200, 200, 40 ,0 , 2 * Math.PI);
+ctx.stroke();
+
+ctx= canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle = "orange";
+ctx.lineWidth = 2;
+ctx.arc(250, 250, 40 ,0 , 2 * Math.PI);
+ctx.stroke();
+
+ctx= canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.lineWidth = 2;
+ctx.arc(300, 200, 40 ,0 , 2 * Math.PI);
+ctx.stroke();
+
+ctx= canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth = 2;
+ctx.arc(350, 250, 40 ,0 , 2 * Math.PI);
+ctx.stroke();
+
+ctx= canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth = 2;
+ctx.arc(400, 200, 40 ,0 , 2 * Math.PI);
+ctx.stroke();
